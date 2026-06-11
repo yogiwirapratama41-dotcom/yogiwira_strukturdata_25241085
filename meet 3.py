57d93c55c6996e3b59a354a7ca41608497154ad1
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2026-06-12 06:01:46 +1000

    Add files via upload

---
+++ meet 3.py
@@ -0,0 +1,12 @@
+# whille loop
+# kodisi
+# aksi
+
+angka =0
+
+while angka < 1:
+    angka += 1
+    print(f"nilai angka:{angka}")
+    print("menyembunyikan Rasa Ke-Seseorang yang entah orang itu sadar akan rasa yang menghantuinya")
+    print("Terkadang Perasaan yang Sengaja Ditahan, Ternyata Membawa Dampak Yang Sangat Tajam dan TAk BERPERASAAN   ")
+print("program ke 3 selse pak")
