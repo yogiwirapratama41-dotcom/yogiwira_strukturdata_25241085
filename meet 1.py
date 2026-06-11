bfbdbe95c2b8a53ff0a99cddc23fd40ea1488b51
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2026-06-12 06:01:06 +1000

    Add files via upload

---
+++ meet 1.py
@@ -0,0 +1,34 @@
+#percabanagan
+#kodisi
+#aksi
+
+nama = input ("masukan nama anda")
+
+if nama == "G1wira":
+    print("selamat datang Yogi")
+    print(" calon Orang Sukses")
+elif nama == "Tora BIKA":
+    print ("hai Kopiku")
+    print("maafff banget yahh,Soalnya Terlalu perfect buatku")
+elif nama == "coppucino":
+    print("hai cino")
+    print("maaf cino anda enak, tapi kurang kalo sama 76 APEL ROYALKU")
+else:
+    print("kamu bukan kopikuu")
+    print("soalnya kamu bikin mah")
+print("program selse")
+
+#jika nama yang di input benar, maka akan munculkan nim kalian
+#jika nama yang di inputkan salah. maka akan memunculkan"nama salah"
+
+nama== input ("masukan nama")
+NIM == input ("masukan pin NIM")
+
+if nama== "YOGI":
+    NIM:"23"
+    print("WELCOM G1wira")
+    print("NO MY NIM 25241085")
+else:
+    print("you name no valid")
+    print("soory your NIM is aecses")    
+
