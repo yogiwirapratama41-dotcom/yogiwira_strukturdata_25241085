f26c104910bd54340492de42d80e476c1ae07f19
Yogi Wira Pratama_Alprog_C <yogiwirapratama41@gmail.com>
2026-06-12 06:01:30 +1000

    Add files via upload

---
+++ meet 2.py
@@ -0,0 +1,16 @@
+# perulangan (loop)
+# for kondisi
+
+angka = [0,1,2,3,4,] # ini menggukan list
+
+for i in angka : 
+    print(f"nilai i = {i}")
+    print("G1wira Nunas Manik Pelungguhm")
+print("program 1 lunas ya pak")
+
+angka = range (1,10) #ini menggunakan range
+
+for i in angka :
+    print (f"nilaii i = {i}")
+    print("Acaa Manieszz Owmakkkk")
+print("program 2 selse pak")
